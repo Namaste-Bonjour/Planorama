d6fb502fc725f4eeea65fca5e5ac30b76f3a6ce8
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-02-17 11:21:51 +0100

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import Navbar from "./components/Navbar";
-import CountryList from "./components/CountryList";
 import Footer from "./components/Footer";
 import React from "react";
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import CreateCountries from "./Pages/CreateCountries";
+import Citylist from "./components/Citylist";
 
 function App() {
   return (
     <>
       <Navbar />
-    <CountryList />
-      <Footer />;
+      <Routes>
+<Route path ="/" element={<HomePage />} />
+<Route path ="/countries/create" element={<CreateCountries />} />
+<Route path ="/countries/:name" element={<Citylist />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
