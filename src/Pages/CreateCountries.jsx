d5cfb5f8c8db90b7a652888009899740955bee0e
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-02-19 15:10:26 +0100

    Fix issue related to styling

--- src/Pages/CreateCountries.jsx
+++ src/Pages/CreateCountries.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../config/api";
-import "./create.css";
+import "./Create.css";
 
 
 
