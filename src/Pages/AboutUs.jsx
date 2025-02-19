2cfe70e60fb966af62c8e4e062feba7a4ad0ff9f
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-02-19 11:50:06 +0100

    add a new page about us

---
+++ src/Pages/AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+function AboutUs() {
+  return (
+    <div>
+      <h1>About us :</h1>
+      <h3>Why this project?</h3>
+      <h2>
+        Welcome to Planorama! We are Sébastien and Anjanaa, two travel
+        enthusiasts who decided to revolutionize the way you plan your
+        adventures. Planorama is a travel planner designed to simplify your life
+        by offering personalized itineraries, practical advice, and authentic
+        discoveries. Our goal is to transform every journey into an
+        unforgettable experience by helping you explore the world with ease and
+        inspiration. Join us and let us guide you in planning your next great
+        adventure!
+      </h2>
+      <p>github:</p>
+    </div>
+  );
+}
+export default AboutUs;
